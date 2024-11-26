73713a32bd0c42543b5a1a7385440241b95c56f6
antlilja <liljaanton2001@gmail.com>
2024-11-26 12:51:39 +0100

    Unitialize correctly

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -40,7 +40,7 @@ extern "C"
 		godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
 		init_obj.register_initializer(initialize_realsense_module);
-		init_obj.register_terminator(initialize_realsense_module);
+		init_obj.register_terminator(uninitialize_realsense_module);
 		init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 
 		return init_obj.init();
